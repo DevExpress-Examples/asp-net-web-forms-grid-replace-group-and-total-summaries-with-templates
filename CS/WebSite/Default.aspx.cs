090427bf8058b0df711214d079e669d0b73969e0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System;
+
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page {
+    protected object GetGroupSummaryValue(GridViewGroupFooterCellTemplateContainer container) {
+        var summaryItem = Grid.GroupSummary.First(i => i.Tag == "GroupVote");
+        return Grid.GetGroupSummaryValue(container.VisibleIndex, summaryItem);
+    }
+    protected object GetTotalSummaryValue() {
+        var summaryItem = Grid.TotalSummary.First(i => i.Tag == "TotalVote");
+        return Grid.GetTotalSummaryValue(summaryItem);
+    }
+}
