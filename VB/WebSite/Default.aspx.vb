99c9ee44497843cb36400fbad00c3ac0f8d7db4d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Linq
+Imports System
+
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Function GetGroupSummaryValue(ByVal container As GridViewGroupFooterCellTemplateContainer) As Object
+		Dim summaryItem = Grid.GroupSummary.First(Function(i) i.Tag = "GroupVote")
+		Return Grid.GetGroupSummaryValue(container.VisibleIndex, summaryItem)
+	End Function
+	Protected Function GetTotalSummaryValue() As Object
+		Dim summaryItem = Grid.TotalSummary.First(Function(i) i.Tag = "TotalVote")
+		Return Grid.GetTotalSummaryValue(summaryItem)
+	End Function
+End Class
